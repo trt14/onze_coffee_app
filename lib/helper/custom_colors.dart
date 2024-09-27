d36377a8bca39a775487080710cdf702fd3dbef8
trt14 <tarooti14dev@gmail.com>
2024-09-27 08:07:34 +0300

    init colors

---
+++ lib/helper/custom_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primary = Color(0xFF3D6B7D);
+  static const Color secondary = Color(0xFFA8483D);
+  static const Color third = Color(0xFFD7D1CA);
+  static const Color forth = Color(0xFF87B1C5);
+}
