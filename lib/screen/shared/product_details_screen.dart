9730ec3afb1f6446e47999705ed556014dea8cb3
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-09-27 16:18:09 +0300

    Design: create product details

--- lib/screen/shared/product_details_screen.dart
+++ lib/screen/shared/product_details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:onze_coffee_app/helper/custom_colors.dart';
+import 'package:onze_coffee_app/helper/screen.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   const ProductDetailsScreen({super.key});
@@ -13,19 +15,51 @@ class ProductDetailsScreen extends StatelessWidget {
             child: Column(
               children: [
                 Image.asset("assets/product/image.png"),
-                Row(
-                  children: [
-                    Expanded(
-                      child: ListTile(
-                        title: Text("Caffe Mocha"),
-                        subtitle: Text("Hot"),
-                      ),
-                    )
-                  ],
+                const ProductTitle(
+                  name: "Coffee Mocha",
+                  cal: "5",
+                  type: "Hot",
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Divider(),
                 )
               ],
             ),
           )),
+    );
+  }
+}
+
+class ProductTitle extends StatelessWidget {
+  const ProductTitle({
+    super.key,
+    required this.name,
+    required this.type,
+    required this.cal,
+  });
+  final String name;
+  final String type;
+  final String cal;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        SizedBox(
+          width: context.getWidth(value: .5),
+          child: ListTile(
+            title: Text(name),
+            subtitle: Text(type),
+          ),
+        ),
+        const Icon(color: AppColor.secondary, Icons.fireplace_outlined),
+        Padding(
+          padding: const EdgeInsets.only(right: 8.0),
+          child: Text(
+              style: const TextStyle(color: AppColor.secondary), "$cal Cal"),
+        )
+      ],
     );
   }
 }
