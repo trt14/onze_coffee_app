19b7e8da7e5a8aa894e8710ca80a87cba41dd5af
trt14 <tarooti14dev@gmail.com>
2024-09-27 08:09:31 +0300

    init folders & screens

---
+++ lib/screen/employee/emp_add_product_scree.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EmpAddProductScree extends StatelessWidget {
+  const EmpAddProductScree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
