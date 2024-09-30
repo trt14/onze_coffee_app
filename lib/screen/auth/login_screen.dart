fe92efd1321b69b7518b3cab6f727e86140ec0a8
FaHaDoF69 <93477293+FaHaDoF69@users.noreply.github.com>
2024-09-30 15:02:41 +0300

    Screens: Create sample login and otp screen for login testing

--- lib/screen/auth/login_screen.dart
+++ lib/screen/auth/login_screen.dart
@@ -1,10 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../cubits/auth_cubit/auth_cubit.dart';
+import '../../data/repositories/auth_repository.dart';
+import 'otp_screen.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  final TextEditingController emailController = TextEditingController();
+
+  LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      create: (context) => AuthCubit(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Login via Email OTP'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Enter your email to receive an OTP',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20),
+              BlocConsumer<AuthCubit, AuthState>(
+                listener: (context, state) {
+                  if (state is AuthLoading) {
+                    // Show a loading spinner or progress bar
+                  } else if (state is AuthSuccess) {
+                    // If OTP sent successfully, navigate to OTP verification screen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => OtpScreen(
+                          email: emailController.text,
+                        ),
+                      ),
+                    );
+                  } else if (state is AuthFailure) {
+                    // Show an error message
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(state.errorMessage)),
+                    );
+                  }
+                },
+                builder: (context, state) {
+                  return state is AuthLoading
+                      ? CircularProgressIndicator()
+                      : ElevatedButton(
+                          onPressed: () {
+                            // Trigger sending OTP to email
+                            context
+                                .read<AuthCubit>()
+                                .sendOtpToEmail(emailController.text);
+                          },
+                          child: Text('Send OTP'),
+                        );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screen/auth/otp_screen.dart
+++ lib/screen/auth/otp_screen.dart
@@ -1,10 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:onze_coffee_app/screen/user/user_home_screen.dart';
+
+import '../../cubits/auth_cubit/auth_cubit.dart';
 
 class OtpScreen extends StatelessWidget {
-  const OtpScreen({super.key});
+  final String email;
+
+  OtpScreen({super.key, required this.email});
+
+  final TextEditingController otpController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      create: (context) => AuthCubit(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Enter OTP'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Please enter the OTP sent to $email'),
+              SizedBox(height: 20),
+              TextField(
+                controller: otpController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'OTP',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20),
+              BlocConsumer<AuthCubit, AuthState>(
+                listener: (context, state) {
+                  if (state is AuthLoading) {
+                    // Show loading indicator
+                  } else if (state is AuthSuccess) {
+                    // OTP verification success, navigate to next screen
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const UserHomeScreen()));
+                  } else if (state is AuthFailure) {
+                    // Show error message
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(state.errorMessage)),
+                    );
+                  }
+                },
+                builder: (context, state) {
+                  return state is AuthLoading
+                      ? CircularProgressIndicator()
+                      : ElevatedButton(
+                          onPressed: () {
+                            context.read<AuthCubit>().verifyEmailOtp(
+                                email: email!, otp: otpController.text);
+                          },
+                          child: Text('Verify OTP'),
+                        );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
