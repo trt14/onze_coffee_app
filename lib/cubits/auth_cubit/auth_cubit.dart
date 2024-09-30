7a9e1aa6a3476119fbc166458cdc9e4ea9856740
FaHaDoF69 <93477293+FaHaDoF69@users.noreply.github.com>
2024-09-30 15:01:17 +0300

    Cubit: Create auth cubit

---
+++ lib/cubits/auth_cubit/auth_cubit.dart
@@ -0,0 +1,51 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../data/repositories/auth_repository.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+
+  /*
+  *
+  * Verify the email OTP
+  *
+  * */
+  Future<void> verifyEmailOtp(
+      {required String email, required String otp}) async {
+    emit(AuthLoading());
+    try {
+      // Call the repository function to verify OTP
+      final result = await AuthRepository().verifyOtp(email: email, otp: otp);
+
+      if (result != '') {
+        // OTP verification successful
+        emit(AuthSuccess());
+      } else {
+        // OTP verification failed
+        emit(AuthFailure('Invalid OTP. Please try again.'));
+      }
+    } catch (error) {
+      // Handle any errors during the process
+      emit(AuthFailure(error.toString()));
+    }
+  }
+
+  /*
+  *
+  * Method to initiate sending OTP (if needed)
+  *
+  * */
+  Future<void> sendOtpToEmail(String email) async {
+    emit(AuthLoading());
+    try {
+      // Call the repository function to send the OTP to the email
+      await AuthRepository().signUp(email: email);
+      emit(AuthSuccess());
+    } catch (error) {
+      emit(AuthFailure('Failed to send OTP. Please try again.'));
+    }
+  }
+}
