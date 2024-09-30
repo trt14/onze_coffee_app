99e2de07d9f407ebbf0f06fecdada69d3fb69186
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-09-30 21:23:24 +0300

    Design: add space

--- lib/screen/shared/orders_screen.dart
+++ lib/screen/shared/orders_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:onze_coffee_app/cubits/order_cubit/order_cubit.dart';
@@ -14,21 +16,21 @@ class OrdersScreen extends StatelessWidget {
       child: Builder(builder: (context) {
         final orderCubit = context.read<OrderCubit>();
         return Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
           child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
+                const Text(
                   "27,December,2024",
                   style: TextStyle(
                       color: AppColor.secondary, fontWeight: FontWeight.w500),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
-                CustomContainerOrder(
+                const CustomContainerOrder(
                     isNote: false,
                     orderID: "217",
                     height: 120,
@@ -37,29 +39,39 @@ class OrdersScreen extends StatelessWidget {
                     textStatus: "Completed",
                     productsWithQuntity:
                         "Coffee Mocha x 1  ,  Coffee Mocha x 2  ,  Coffee Mocha x 1"),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
 
                 BlocBuilder<OrderCubit, OrderState>(
                   builder: (context, state) {
-                    return Column(
-                      children:
-                          List.generate(orderCubit.bills.length, (int index) {
-                        return CustomContainerOrder(
-                            isNote: orderCubit.bills[index].note.isNotEmpty,
-                            orderID: orderCubit.bills[index].billId.toString(),
-                            height: 120,
-                            width: 1,
-                            price:
-                                orderCubit.bills[index].totalPrice.toDouble(),
-                            statusColor: AppColor.forth,
-                            note: orderCubit.bills[index].note,
-                            textStatus: orderCubit.bills[index].status,
-                            productsWithQuntity:
-                                "Coffee Mocha x 1  ,  Coffee Mocha x 2  ,  Coffee Mocha x 1");
+                    return Column(children: [
+                      ...List.generate(orderCubit.bills.length, (int index) {
+                        return Column(
+                          children: [
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            CustomContainerOrder(
+                                isNote: orderCubit.bills[index].note.isNotEmpty,
+                                orderID:
+                                    orderCubit.bills[index].billId.toString(),
+                                height: 120,
+                                width: 1,
+                                price: orderCubit.bills[index].totalPrice
+                                    .toDouble(),
+                                statusColor: AppColor.forth,
+                                note: orderCubit.bills[index].note,
+                                textStatus: orderCubit.bills[index].status,
+                                productsWithQuntity: orderCubit
+                                    .bills[index].products
+                                    .map((element) =>
+                                        "${element.name} x${element.qty}")
+                                    .toString()),
+                          ],
+                        );
                       }),
-                    );
+                    ]);
                   },
                 ),
 
